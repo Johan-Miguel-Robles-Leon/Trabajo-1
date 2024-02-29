e441da4e6b9a5148ec013e85d097df309a581b48
Johan-Miguel-Robles-Leon <johanblue15@gmail.com>
2024-02-29 12:22:03 -0700

    mama

--- src/app/tercer-dia/tercer-dia.component.ts
+++ src/app/tercer-dia/tercer-dia.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { ExampleService } from '../services/example.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tercer-dia',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './tercer-dia.component.html',
   styleUrl: './tercer-dia.component.css'
 })
 export class TercerDiaComponent {
-
+  name ='';
+  users: any =[]
+  constructor(public example: ExampleService){
+  }
+  ngOnInit() {
+  this.example.obtenerLista().subscribe(resp =>{
+    console.log(resp);
+    this.users = resp;
+    })
+  }
 }
